5e149521c865926cca887b5345a314c3a6531172
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-06-14 22:01:57 -0400

    Remove comment

--- lib/gravity_mailbox/engine.rb
+++ lib/gravity_mailbox/engine.rb
@@ -14,23 +14,5 @@ module GravityMailbox
         )
       end
     end
-
-    # routes.draw do
-    #   puts "\n#{__FILE__ }:#{__LINE__}\n\n"
-    #   get '/' => 'gravity_mailbox/mailbox#index'
-    #   get '/download_eml' => 'gravity_mailbox/mailbox#download_eml'
-    #   post '/delete' => 'gravity_mailbox/mailbox#delete'
-    #   post '/delete_all' => 'gravity_mailbox/mailbox#delete_all'
-    # end
-    #
-
-    # config.after_initialize do |app|
-    #   app.routes.prepend do
-    #     get '/gravity_mailbox' => 'gravity_mailbox/mailbox#index', internal: true
-    #     post '/gravity_mailbox/delete_all' => 'gravity_mailbox/mailbox#delete_all', internal: true
-    #     get '/gravity_mailbox/download_eml' => 'gravity_mailbox/mailbox#download_eml', internal: true
-    #     post '/gravity_mailbox/delete' => 'gravity_mailbox/mailbox#delete', internal: true
-    #   end
-    # end
   end
 end
